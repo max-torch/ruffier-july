f0bce81369062791cb7c8a9ea612f9a6c207cc81
jezoiah <jezoiah@gmail.com>
2025-08-15 15:34:34 +0800

    Remove unused button click handler in event.py
    Deleted the on_button_clicked2 function and its connection to screen2.result, as they are no longer used. This simplifies the event handling logic.

--- event.py
+++ event.py
@@ -19,16 +19,12 @@ import screen2 as scr2
 def on_button_clicked():
     screen2.show()
     screen1.hide()
-def on_button_clicked2():
-    screen3.show()
-    screen2.hide()
 
 
 app = QApplication([])
 screen1 = scr1.Screen1()
 screen2 = scr2.Screen2()
 screen1.button.clicked.connect(on_button_clicked)
-screen2.result.clicked.connect(on_button_clicked2)
 
 screen1.show()
 app.exec_()
